830bf4c4adb2a57926ae91fe6b43592be71207a8
AlmasikkMe <aljmasikplayl1tttdt3@gmail.com>
2026-06-09 22:46:56 +0500

    feat: Добавить базовые модели Message и типы сообщений в Handler

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿public class Handler
+{
+    public static Dictionary<string, (string Emoji, string Name, bool IsWithTime)> MessagesTypes = new()
+    {
+        { "photo",        ("🖼", "Фотография",          false) },
+        { "video",        ("📹", "Видеозапись",         false) },
+        { "video_note",   ("📹", "Видеосообщение",      true)  },
+        { "gif",          ("💥", "GIF",                 false) },
+        { "voice",        ("🎤", "Голосовое сообщение", true)  },
+        { "audio",        ("🎶", "Аудиозапись",         false) },
+        { "voice_effect", ("🔊", "Голосовой эффект",    false) },
+        { "file",         ("📂", "Файл",                false) },
+        { "sticker",      ("🎨", "Стикер",              false) },
+        { "emoji",        ("🎨", "Эмодзи",              false) },
+        { "poll",         ("📊", "Опрос",               false) },
+        { "quiz",         ("📊", "Викторина",           false) },
+        { "contact",      ("👤", "Контакт",             false) },
+        { "location",     ("📍", "Геолокация",          false) },
+        { "live_location",("🚨", "Живая геолокация",    false) },
+        { "game",         ("🎮", "Игра",                false) },
+        { "product",      ("🛍", "Продукт",             false) },
+        { "gift",         ("🎁", "Подарок",             false) }
+    };
+}
+
+public class Message(string sender, string text, string type = "text")
+{
+    public string Sender = sender;
+    public string Text = text;
+    public string Type = type;
+    public DateTime DateTime;
+}
