f6b2e3773ef07b8bb84b5271163ec5bea84b2cff
AlmasikkMe <aljmasikplayl1tttdt3@gmail.com>
2026-06-12 20:37:33 +0500

    feat: Добавлено вожможность создавать контакты и группы

--- Program.cs
+++ Program.cs
@@ -1,4 +1,52 @@
-﻿public class Handler
+﻿Messager messager = new();
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Что вы хотите сделать?");
+    Console.WriteLine("1. Создать контакт");
+    Console.WriteLine("2. Создать группу");
+    Console.WriteLine("3. Перейти в чат");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            NewContact();
+            break;
+        case "2":
+            NewGroup();
+            break;
+        case "3":
+            break;
+    }
+}
+
+void NewContact()
+{
+    Console.Write("Введите имя для контакта: ");
+    string? contact = Console.ReadLine();
+    if (contact == null) Console.WriteLine();
+    messager.NewContact(contact ?? "");
+}
+
+void NewGroup()
+{
+    Console.Write("Выберите имена членов группы (через запятую):");
+
+    string[] members;
+    string? userInput = Console.ReadLine();
+    if (!string.IsNullOrWhiteSpace(userInput))
+    {
+        members = userInput.Split(",");
+    }
+
+    Console.Write("Введите имя для группы: ");
+    userInput = Console.ReadLine();
+    if (userInput == null) Console.WriteLine();
+}
+
+
+
+public class Messager
 {
     public static Dictionary<string, (string Emoji, string Name, bool IsWithTime)> MessagesTypes = new()
     {
@@ -21,6 +69,53 @@
         { "product",      ("🛍", "Продукт",             false) },
         { "gift",         ("🎁", "Подарок",             false) }
     };
+    private List<string> Contacts = [];
+    private List<Chat> Chats = [];
+
+    public void NewContact(string contactName)
+    {
+        contactName = contactName.IsWhiteSpace() ? $"Контакт {Contacts.Count + 1}" : contactName.Trim();
+        Contacts.Add(contactName);
+        Chats.Add(new(contactName) { Members = [contactName] });
+    }
+
+    public void NewGroup(string groupName, string[] members)
+    {
+        if (members.Length == 0) return;
+
+        List<string> membersList = [];
+
+        for (int i = 0; i < members.Length; i++)
+        {
+            membersList.Add(members[i].Trim());
+        }
+
+        if (groupName.IsWhiteSpace()) 
+        {
+            groupName = "";
+
+            for (int i = 0; i < 5 && i < membersList.Count; i++)
+            {
+                if (i == 4)
+                {
+                    groupName += $"еще {membersList.Count - i + 1}";
+                    break;
+                }
+
+                groupName += membersList[i];
+
+                if (i >= membersList.Count - 1) groupName += ", ";
+            }
+        }
+
+        Chats.Add(new(groupName) { Members = membersList });
+    }
+}
+
+public class Chat(string chatName)
+{
+    public string ChatName = chatName;
+    public List<string> Members = [];
 }
 
 public class Message(string sender, string text, string type = "text")
